a0ba80c74c53c3189858a3ea79fa4e37c0dd56e3
Sofia Rey <sofurey88@gmail.com>
2020-05-29 18:22:34 -0300

    Escalerización realizada

---
+++ WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,365 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtbxA = New System.Windows.Forms.TextBox()
+        Me.txtbxB = New System.Windows.Forms.TextBox()
+        Me.txtbxC = New System.Windows.Forms.TextBox()
+        Me.txtbxF = New System.Windows.Forms.TextBox()
+        Me.txtbxE = New System.Windows.Forms.TextBox()
+        Me.txtbxD = New System.Windows.Forms.TextBox()
+        Me.txtbxI = New System.Windows.Forms.TextBox()
+        Me.txtbxH = New System.Windows.Forms.TextBox()
+        Me.txtbxG = New System.Windows.Forms.TextBox()
+        Me.btnResult = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblA = New System.Windows.Forms.Label()
+        Me.lblB = New System.Windows.Forms.Label()
+        Me.lblC = New System.Windows.Forms.Label()
+        Me.txtbxZ = New System.Windows.Forms.TextBox()
+        Me.txtbxY = New System.Windows.Forms.TextBox()
+        Me.txtbxX = New System.Windows.Forms.TextBox()
+        Me.lblX = New System.Windows.Forms.Label()
+        Me.lblY = New System.Windows.Forms.Label()
+        Me.lblF = New System.Windows.Forms.Label()
+        Me.lblE = New System.Windows.Forms.Label()
+        Me.lblD = New System.Windows.Forms.Label()
+        Me.lblZ = New System.Windows.Forms.Label()
+        Me.lblI = New System.Windows.Forms.Label()
+        Me.lblH = New System.Windows.Forms.Label()
+        Me.lblG = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtbxA
+        '
+        Me.txtbxA.Location = New System.Drawing.Point(309, 104)
+        Me.txtbxA.Name = "txtbxA"
+        Me.txtbxA.Size = New System.Drawing.Size(35, 20)
+        Me.txtbxA.TabIndex = 0
+        '
+        'txtbxB
+        '
+        Me.txtbxB.Location = New System.Drawing.Point(359, 104)
+        Me.txtbxB.Name = "txtbxB"
+        Me.txtbxB.Size = New System.Drawing.Size(35, 20)
+        Me.txtbxB.TabIndex = 1
+        '
+        'txtbxC
+        '
+        Me.txtbxC.Location = New System.Drawing.Point(410, 104)
+        Me.txtbxC.Name = "txtbxC"
+        Me.txtbxC.Size = New System.Drawing.Size(35, 20)
+        Me.txtbxC.TabIndex = 2
+        '
+        'txtbxF
+        '
+        Me.txtbxF.Location = New System.Drawing.Point(410, 130)
+        Me.txtbxF.Name = "txtbxF"
+        Me.txtbxF.Size = New System.Drawing.Size(35, 20)
+        Me.txtbxF.TabIndex = 5
+        '
+        'txtbxE
+        '
+        Me.txtbxE.Location = New System.Drawing.Point(359, 130)
+        Me.txtbxE.Name = "txtbxE"
+        Me.txtbxE.Size = New System.Drawing.Size(35, 20)
+        Me.txtbxE.TabIndex = 4
+        '
+        'txtbxD
+        '
+        Me.txtbxD.Location = New System.Drawing.Point(309, 130)
+        Me.txtbxD.Name = "txtbxD"
+        Me.txtbxD.Size = New System.Drawing.Size(35, 20)
+        Me.txtbxD.TabIndex = 3
+        '
+        'txtbxI
+        '
+        Me.txtbxI.Location = New System.Drawing.Point(410, 156)
+        Me.txtbxI.Name = "txtbxI"
+        Me.txtbxI.Size = New System.Drawing.Size(35, 20)
+        Me.txtbxI.TabIndex = 8
+        '
+        'txtbxH
+        '
+        Me.txtbxH.Location = New System.Drawing.Point(359, 156)
+        Me.txtbxH.Name = "txtbxH"
+        Me.txtbxH.Size = New System.Drawing.Size(35, 20)
+        Me.txtbxH.TabIndex = 7
+        '
+        'txtbxG
+        '
+        Me.txtbxG.Location = New System.Drawing.Point(309, 156)
+        Me.txtbxG.Name = "txtbxG"
+        Me.txtbxG.Size = New System.Drawing.Size(35, 20)
+        Me.txtbxG.TabIndex = 6
+        '
+        'btnResult
+        '
+        Me.btnResult.Location = New System.Drawing.Point(358, 199)
+        Me.btnResult.Name = "btnResult"
+        Me.btnResult.Size = New System.Drawing.Size(76, 29)
+        Me.btnResult.TabIndex = 9
+        Me.btnResult.Text = "Resultado"
+        Me.btnResult.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
+        Me.Label1.Location = New System.Drawing.Point(252, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(322, 31)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Esclerización de matrices"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Label2.Location = New System.Drawing.Point(379, 73)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(52, 20)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Matriz"
+        '
+        'lblA
+        '
+        Me.lblA.AutoSize = True
+        Me.lblA.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.lblA.Location = New System.Drawing.Point(354, 258)
+        Me.lblA.Name = "lblA"
+        Me.lblA.Size = New System.Drawing.Size(14, 20)
+        Me.lblA.TabIndex = 12
+        Me.lblA.Text = "-"
+        '
+        'lblB
+        '
+        Me.lblB.AutoSize = True
+        Me.lblB.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.lblB.Location = New System.Drawing.Point(378, 258)
+        Me.lblB.Name = "lblB"
+        Me.lblB.Size = New System.Drawing.Size(14, 20)
+        Me.lblB.TabIndex = 13
+        Me.lblB.Text = "-"
+        '
+        'lblC
+        '
+        Me.lblC.AutoSize = True
+        Me.lblC.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.lblC.Location = New System.Drawing.Point(403, 258)
+        Me.lblC.Name = "lblC"
+        Me.lblC.Size = New System.Drawing.Size(14, 20)
+        Me.lblC.TabIndex = 14
+        Me.lblC.Text = "-"
+        '
+        'txtbxZ
+        '
+        Me.txtbxZ.Location = New System.Drawing.Point(462, 156)
+        Me.txtbxZ.Name = "txtbxZ"
+        Me.txtbxZ.Size = New System.Drawing.Size(35, 20)
+        Me.txtbxZ.TabIndex = 17
+        '
+        'txtbxY
+        '
+        Me.txtbxY.Location = New System.Drawing.Point(462, 130)
+        Me.txtbxY.Name = "txtbxY"
+        Me.txtbxY.Size = New System.Drawing.Size(35, 20)
+        Me.txtbxY.TabIndex = 16
+        '
+        'txtbxX
+        '
+        Me.txtbxX.Location = New System.Drawing.Point(462, 104)
+        Me.txtbxX.Name = "txtbxX"
+        Me.txtbxX.Size = New System.Drawing.Size(35, 20)
+        Me.txtbxX.TabIndex = 15
+        '
+        'lblX
+        '
+        Me.lblX.AutoSize = True
+        Me.lblX.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.lblX.Location = New System.Drawing.Point(424, 258)
+        Me.lblX.Name = "lblX"
+        Me.lblX.Size = New System.Drawing.Size(14, 20)
+        Me.lblX.TabIndex = 18
+        Me.lblX.Text = "-"
+        '
+        'lblY
+        '
+        Me.lblY.AutoSize = True
+        Me.lblY.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.lblY.Location = New System.Drawing.Point(424, 278)
+        Me.lblY.Name = "lblY"
+        Me.lblY.Size = New System.Drawing.Size(14, 20)
+        Me.lblY.TabIndex = 22
+        Me.lblY.Text = "-"
+        '
+        'lblF
+        '
+        Me.lblF.AutoSize = True
+        Me.lblF.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.lblF.Location = New System.Drawing.Point(404, 278)
+        Me.lblF.Name = "lblF"
+        Me.lblF.Size = New System.Drawing.Size(14, 20)
+        Me.lblF.TabIndex = 21
+        Me.lblF.Text = "-"
+        '
+        'lblE
+        '
+        Me.lblE.AutoSize = True
+        Me.lblE.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.lblE.Location = New System.Drawing.Point(379, 278)
+        Me.lblE.Name = "lblE"
+        Me.lblE.Size = New System.Drawing.Size(14, 20)
+        Me.lblE.TabIndex = 20
+        Me.lblE.Text = "-"
+        '
+        'lblD
+        '
+        Me.lblD.AutoSize = True
+        Me.lblD.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.lblD.Location = New System.Drawing.Point(355, 278)
+        Me.lblD.Name = "lblD"
+        Me.lblD.Size = New System.Drawing.Size(14, 20)
+        Me.lblD.TabIndex = 19
+        Me.lblD.Text = "-"
+        '
+        'lblZ
+        '
+        Me.lblZ.AutoSize = True
+        Me.lblZ.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.lblZ.Location = New System.Drawing.Point(424, 298)
+        Me.lblZ.Name = "lblZ"
+        Me.lblZ.Size = New System.Drawing.Size(14, 20)
+        Me.lblZ.TabIndex = 26
+        Me.lblZ.Text = "-"
+        '
+        'lblI
+        '
+        Me.lblI.AutoSize = True
+        Me.lblI.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.lblI.Location = New System.Drawing.Point(404, 298)
+        Me.lblI.Name = "lblI"
+        Me.lblI.Size = New System.Drawing.Size(14, 20)
+        Me.lblI.TabIndex = 25
+        Me.lblI.Text = "-"
+        '
+        'lblH
+        '
+        Me.lblH.AutoSize = True
+        Me.lblH.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.lblH.Location = New System.Drawing.Point(379, 298)
+        Me.lblH.Name = "lblH"
+        Me.lblH.Size = New System.Drawing.Size(14, 20)
+        Me.lblH.TabIndex = 24
+        Me.lblH.Text = "-"
+        '
+        'lblG
+        '
+        Me.lblG.AutoSize = True
+        Me.lblG.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.lblG.Location = New System.Drawing.Point(355, 298)
+        Me.lblG.Name = "lblG"
+        Me.lblG.Size = New System.Drawing.Size(14, 20)
+        Me.lblG.TabIndex = 23
+        Me.lblG.Text = "-"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(353, 329)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.TabIndex = 27
+        Me.Label3.Text = "Label3"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lblZ)
+        Me.Controls.Add(Me.lblI)
+        Me.Controls.Add(Me.lblH)
+        Me.Controls.Add(Me.lblG)
+        Me.Controls.Add(Me.lblY)
+        Me.Controls.Add(Me.lblF)
+        Me.Controls.Add(Me.lblE)
+        Me.Controls.Add(Me.lblD)
+        Me.Controls.Add(Me.lblX)
+        Me.Controls.Add(Me.txtbxZ)
+        Me.Controls.Add(Me.txtbxY)
+        Me.Controls.Add(Me.txtbxX)
+        Me.Controls.Add(Me.lblC)
+        Me.Controls.Add(Me.lblB)
+        Me.Controls.Add(Me.lblA)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnResult)
+        Me.Controls.Add(Me.txtbxI)
+        Me.Controls.Add(Me.txtbxH)
+        Me.Controls.Add(Me.txtbxG)
+        Me.Controls.Add(Me.txtbxF)
+        Me.Controls.Add(Me.txtbxE)
+        Me.Controls.Add(Me.txtbxD)
+        Me.Controls.Add(Me.txtbxC)
+        Me.Controls.Add(Me.txtbxB)
+        Me.Controls.Add(Me.txtbxA)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtbxA As TextBox
+    Friend WithEvents txtbxB As TextBox
+    Friend WithEvents txtbxC As TextBox
+    Friend WithEvents txtbxF As TextBox
+    Friend WithEvents txtbxE As TextBox
+    Friend WithEvents txtbxD As TextBox
+    Friend WithEvents txtbxI As TextBox
+    Friend WithEvents txtbxH As TextBox
+    Friend WithEvents txtbxG As TextBox
+    Friend WithEvents btnResult As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblA As Label
+    Friend WithEvents lblB As Label
+    Friend WithEvents lblC As Label
+    Friend WithEvents txtbxZ As TextBox
+    Friend WithEvents txtbxY As TextBox
+    Friend WithEvents txtbxX As TextBox
+    Friend WithEvents lblX As Label
+    Friend WithEvents lblY As Label
+    Friend WithEvents lblF As Label
+    Friend WithEvents lblE As Label
+    Friend WithEvents lblD As Label
+    Friend WithEvents lblZ As Label
+    Friend WithEvents lblI As Label
+    Friend WithEvents lblH As Label
+    Friend WithEvents lblG As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,113 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, j As EventArgs) Handles btnResult.Click
+        'Casillas de matriz ingresada
+        'Fila 1
+        Dim a As Double = txtbxA.Text
+        Dim b As Double = txtbxB.Text
+        Dim c As Double = txtbxC.Text
+        Dim x As Double = txtbxX.Text
+        'Fila 2
+        Dim d As Double = txtbxD.Text
+        Dim e As Double = txtbxE.Text
+        Dim f As Double = txtbxF.Text
+        Dim y As Double = txtbxY.Text
+        'Fila 3
+        Dim g As Double = txtbxG.Text
+        Dim h As Double = txtbxH.Text
+        Dim i As Double = txtbxI.Text
+        Dim z As Double = txtbxZ.Text
+
+        'Columna 1
+        'Fila1/a (casilla a = 1)
+        b = b / a
+        c = c / a
+        x = x / a
+        a = a / a
+
+        'Fila2 - d(Fila1) (casilla d = 0)
+        e = e - (d * b)
+        f = f - (d * c)
+        y = y - (d * x)
+        d = d - (d * a)
+
+        'Fila3 - g(Fila1) (casilla g = 0)
+        h = h - (g * b)
+        i = i - (g * c)
+        z = z - (g * x)
+        g = g - (g * a)
+
+        'Columna 2
+        'Fila2/e (casilla e = 1)
+        d = d / e
+        f = f / e
+        y = y / e
+        e = e / e
+
+        'Fila3 - h(Fila2) (casilla h = 0)
+        g = g - (h * d)
+        i = i - (h * f)
+        z = z - (h * y)
+        h = h - (h * e)
+
+        'Fila1 - b(Fila2) (casilla b = 0)
+        a = a - (b * d)
+        c = c - (b * f)
+        x = x - (b * y)
+        b = b - (b * e)
+
+        'Columna 3
+        'Fila3/i (casilla i = 1)
+        g = g / i
+        h = h / i
+        z = z / i
+        i = i / i
+
+        'Fila2 - f(Fila3) (casilla f = 0)
+        d = d - (f * g)
+        e = e - (f * h)
+        y = y - (f * z)
+        f = f - (f * i)
+
+        'Fila1 - c(Fila3) (casilla c = 0)
+        a = a - (c * g)
+        b = b - (c * h)
+        x = x - (c * z)
+        c = c - (c * i)
+
+
+
+        'Mostrar resultados
+        lblA.Text = a
+        lblB.Text = b
+        lblC.Text = c
+        lblX.Text = x
+        lblD.Text = d
+        lblE.Text = e
+        lblF.Text = f
+        lblY.Text = y
+        lblG.Text = g
+        lblH.Text = h
+        lblI.Text = i
+        lblZ.Text = z
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub TextBox5_TextChanged(sender As Object, e As EventArgs) Handles txtbxE.TextChanged
+
+    End Sub
+
+    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles txtbxF.TextChanged
+
+    End Sub
+
+    Private Sub txtbxA_TextChanged(sender As Object, e As EventArgs) Handles txtbxA.TextChanged
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+End Class
